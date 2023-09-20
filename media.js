0eaab865c2f73ee82a44de8028f3cf0e25a6f67a
Pedrindelas2410 <116369128+Pedrindelas2410@users.noreply.github.com>
2023-09-20 09:10:29 -0300

    Add files via upload

---
+++ media.js
@@ -0,0 +1,13 @@
+//Cria referencia ao form e elementos de resposta do programa
+
+const form = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+//cria um ouvinte
+form.addEventListener9("submit",(e) =>){e.preventDefault()
+const nome = form.inNome.value
+const nota = Number(form.inNota1.value)
+const nota = Number(form.inNota2.value)
+const media = (nota1 + nota2.value)
+}
