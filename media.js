9a513089ff2423b600cdb5100b35f49e90bce2fb
Pedrindelas2410 <116369128+Pedrindelas2410@users.noreply.github.com>
2023-09-25 11:57:19 -0300

    Add files via upload

--- media.js
+++ media.js
@@ -6,8 +6,30 @@ const resp2 = document.querySelector("h4")
 
 //cria um ouvinte
 form.addEventListener9("submit",(e) =>){e.preventDefault()
+    e.preventDefault()
+
 const nome = form.inNome.value
 const nota = Number(form.inNota1.value)
 const nota = Number(form.inNota2.value)
 const media = (nota1 + nota2.value)
+
+const media = (nota1 + nota2)/2
+resp1.innerText =media das notas ${media.toFixed(2)}
+
+if(media >= 7) {
+    resp2.innerText = `Parabens ${nome}! Voçe foi aprovado(a)`
+    resp2.style.cor = "blue"
+    else if (media >=4) {
+        resp2.innerText =`Atenção ${nome}!Voçe esta de exame`
+        resp2.style.color ="green"
+        else(
+            resp2.innerText =`Ops $(nome)! Voçe foi reprovado(a)`
+            resp2.style.color ="red"
+        )
+    }
+}
+
+
+
+
 }
