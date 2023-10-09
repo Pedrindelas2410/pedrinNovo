bf2847ce4ceee5ca22514dcbff66fb8d474702d4
Pedrindelas2410 <116369128+Pedrindelas2410@users.noreply.github.com>
2023-10-09 12:00:15 -0300

    Add files via upload

--- media.js
+++ media.js
@@ -1,35 +1,25 @@
-//Cria referencia ao form e elementos de resposta do programa
-
 const form = document.querySelector("form")
 const resp1 = document.querySelector("h3")
 const resp2 = document.querySelector("h4")
 
-//cria um ouvinte
 form.addEventListener9("submit",(e) =>){e.preventDefault()
     e.preventDefault()
 
 const nome = form.inNome.value
-const nota = Number(form.inNota1.value)
-const nota = Number(form.inNota2.value)
+const nota1 = Number(form.inNota1.value)
+const nota2 = Number(form.inNota2.value)
 const media = (nota1 + nota2.value)
-
 const media = (nota1 + nota2)/2
-resp1.innerText =media das notas ${media.toFixed(2)}
+resp1.innerText = `media das notas ${media.toFixed(2)}`
 
 if(media >= 7) {
     resp2.innerText = `Parabens ${nome}! Voçe foi aprovado(a)`
     resp2.style.cor = "blue"
-    else if (media >=4) {
+ } else if (media >=4) {
         resp2.innerText =`Atenção ${nome}!Voçe esta de exame`
         resp2.style.color ="green"
-        else(
+  } else{
             resp2.innerText =`Ops $(nome)! Voçe foi reprovado(a)`
             resp2.style.color ="red"
-        )
     }
-}
-
-
-
-
 }
